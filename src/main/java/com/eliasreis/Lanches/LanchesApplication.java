88b25366e8897e46aff022be2c34411e41c3910d
Elias Reis <eliasreis54@gmail.com>
2018-04-14 18:50:30 -0300

    heroku files

--- src/main/java/com/eliasreis/Lanches/LanchesApplication.java
+++ src/main/java/com/eliasreis/Lanches/LanchesApplication.java
@@ -23,14 +23,8 @@ public class LanchesApplication {
 	public CommandLineRunner commandLineRunner() {
 		return args -> {
 			
-			Usuario usuario = new Usuario();
-			usuario.setEmail("usuario@email.com");
-			usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
-			usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
-			this.usuarioRepository.save(usuario);
-			
 			Usuario admin = new Usuario();
-			admin.setEmail("admin@email.com");
+			admin.setEmail("teste@email.com");
 			admin.setPerfil(PerfilEnum.ROLE_ADMIN);
 			admin.setSenha(SenhaUtils.gerarBCrypt("123456"));
 			this.usuarioRepository.save(admin);
