d84c0a4cd4befee83288a916f5d4cca8a405662b
Elias Reis <eliasreis54@gmail.com>
2018-04-14 18:34:17 -0300

    push heroku

--- src/main/java/com/eliasreis/Lanches/LanchesApplication.java
+++ src/main/java/com/eliasreis/Lanches/LanchesApplication.java
@@ -13,8 +13,28 @@ import com.eliasreis.Lanches.security.repositories.UsuarioRepository;
 
 @SpringBootApplication
 public class LanchesApplication {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(LanchesApplication.class, args);
 	}
-	
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			
+			Usuario usuario = new Usuario();
+			usuario.setEmail("usuario@email.com");
+			usuario.setPerfil(PerfilEnum.ROLE_USUARIO);
+			usuario.setSenha(SenhaUtils.gerarBCrypt("123456"));
+			this.usuarioRepository.save(usuario);
+			
+			Usuario admin = new Usuario();
+			admin.setEmail("admin@email.com");
+			admin.setPerfil(PerfilEnum.ROLE_ADMIN);
+			admin.setSenha(SenhaUtils.gerarBCrypt("123456"));
+			this.usuarioRepository.save(admin);
+			
+		};
+	}
 }
