da08846243cd3c3f151b1adc0048db67e26023fb
Elias Reis <eliasreis54@gmail.com>
2018-04-16 23:59:28 -0300

    Heroku files 1.8

--- src/main/java/com/eliasreis/Lanches/security/config/WebSecurityConfig.java
+++ src/main/java/com/eliasreis/Lanches/security/config/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.eliasreis.Lanches.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -51,6 +52,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/auth/**").permitAll().anyRequest().authenticated();
 		httpSecurity.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
 		httpSecurity.headers().cacheControl();
+		httpSecurity.exceptionHandling().and().authorizeRequests().antMatchers(HttpMethod.OPTIONS).permitAll().antMatchers("/**")
+		.authenticated().and().httpBasic();
 	}
 
 }
