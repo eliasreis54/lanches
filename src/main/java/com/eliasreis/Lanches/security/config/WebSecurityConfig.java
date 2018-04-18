08aab22d25445bfc68cbea7b287c5da441446975
Elias Reis <eliasreis54@gmail.com>
2018-04-17 23:44:12 -0300

    heroku files v3.2

--- src/main/java/com/eliasreis/Lanches/security/config/WebSecurityConfig.java
+++ src/main/java/com/eliasreis/Lanches/security/config/WebSecurityConfig.java
@@ -53,7 +53,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	 @Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost/"));
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost"));
 		configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT"));
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
