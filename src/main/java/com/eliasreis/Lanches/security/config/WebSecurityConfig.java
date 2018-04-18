f1903614f3391947a059e5f427f2e583ba11c7b0
Elias Reis <eliasreis54@gmail.com>
2018-04-18 00:08:14 -0300

    heroku files v3.9

--- src/main/java/com/eliasreis/Lanches/security/config/WebSecurityConfig.java
+++ src/main/java/com/eliasreis/Lanches/security/config/WebSecurityConfig.java
@@ -49,7 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 	
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
 	}
 
 	@Bean
